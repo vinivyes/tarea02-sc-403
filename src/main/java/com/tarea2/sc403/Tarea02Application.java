0e47c13b02cc6ff871f876941a3d088ebbf24db6
Vinicius de Morais <viniciusmorais352@gmail.com>
2022-06-04 12:17:24 -0600

    Tarea02 - Inicio

---
+++ src/main/java/com/tarea2/sc403/Tarea02Application.java
@@ -0,0 +1,13 @@
+package com.tarea2.sc403;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarea2/sc403/Tarea02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea2.sc403;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
